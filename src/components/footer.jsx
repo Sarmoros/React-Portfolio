1c9529873b0a2593ca1d98b64529a0b3456440c4
Sarahy <sarahym0703@gmail.com>
2024-03-24 21:24:47 -0400

    added personal links to ig, linkedin, and github. I also removed the twitter link since I do not have twitter

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,10 +4,9 @@ const Footer = () => {
     return (
         <footer>
             <ul className="footer-links" style={{ listStyle: 'none', padding: 0, display: 'flex', justifyContent: 'center' }}>
-                <li style={{ margin: '0 10px' }}><a href="https://github.com"><i className="fab fa-github"></i> GitHub</a></li>
-                <li style={{ margin: '0 10px' }}><a href="https://linkedin.com"><i className="fab fa-linkedin"></i> LinkedIn</a></li>
-                <li style={{ margin: '0 10px' }}><a href="https://twitter.com"><i className="fab fa-twitter"></i> Twitter</a></li>
-                <li style={{ margin: '0 10px' }}><a href="https://instagram.com"><i className="fab fa-instagram"></i> Instagram</a></li>
+                <li style={{ margin: '0 10px' }}><a href="https://github.com/Sarmoros"><i className="fab fa-github"></i> GitHub</a></li>
+                <li style={{ margin: '0 10px' }}><a href="https://www.linkedin.com/in/sarahy-m-25324627b/"><i className="fab fa-linkedin"></i> LinkedIn</a></li>
+                <li style={{ margin: '0 10px' }}><a href="https://www.instagram.com/sarahy.ta/"><i className="fab fa-instagram"></i> Instagram</a></li>
             </ul>
         </footer>
     );
